f6ed1913aa2489825f62dfec36b5c126522b097e
ngrudenko <ngrudenko@yamoney.ru>
2017-10-26 15:29:01 +0300

    workshop #2 done

--- src/client/ex.2.js
+++ src/client/ex.2.js
@@ -20,3 +20,45 @@
 //
 // Nyan cat lies here...
 //
+
+const result = document.getElementsByClassName('result')[0];
+const input = document.getElementsByClassName('text')[0];
+const sendBtn = document.getElementsByClassName('send')[0];
+
+const socket = io('http://ws.rudenko.tech', {
+	path: '/nodeschool/socket.io',
+	transports: ['websocket']
+});
+
+socket.on('connect', () => {
+	write(`connect`);
+});
+
+socket.on('message', data => {
+	write(data);
+});
+
+socket.on('connect_error', () => {
+	write('connect_error');
+});
+
+socket.on('disconnect', reason => {
+	write(`disconnect ${reason}`);
+});
+
+socket.on('ping', () => {
+	write('ping');
+});
+
+socket.on('pong', () => {
+	write('pong');
+});
+
+sendBtn.addEventListener('click', () => {
+	const text = input.value;
+	socket.emit('message', text);
+});
+
+function write(text) {
+	result.innerHTML += `${text}<br/>`;
+}
