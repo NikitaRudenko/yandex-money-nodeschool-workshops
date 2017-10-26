2a2423959aab219cfc2422c53d8e9144c6d65479
ngrudenko <ngrudenko@yamoney.ru>
2017-10-26 15:27:14 +0300

    workshop #1 done

--- src/client/ex.1.js
+++ src/client/ex.1.js
@@ -20,3 +20,36 @@
 //
 // Nyan cat lies here...
 //
+
+const result = document.getElementsByClassName('result')[0];
+const input = document.getElementsByClassName('text')[0];
+const sendBtn = document.getElementsByClassName('send')[0];
+
+const ws = new WebSocket('ws://ws.rudenko.tech/nodeschool/ws');
+
+ws.onopen = () => {
+	write('Open');
+};
+
+ws.onerror = error => {
+	write(`Error: ${error}`);
+};
+
+ws.onmessage = e => {
+	const message = e.data;
+	write(`Message: ${message}`);
+};
+
+ws.onclose = e => {
+	const wasClean = e.wasClean;
+	write(`Close wasClean: ${wasClean}`);
+};
+
+sendBtn.addEventListener('click', () => {
+	const text = input.value;
+	ws.send(text);
+});
+
+function write(text) {
+	result.innerHTML += `${text}<br/>`;
+}
